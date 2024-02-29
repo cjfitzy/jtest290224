01b1593a846cace12208bcb32da2e5eecbb577ba
cjfitzy <140505765+cjfitzy@users.noreply.github.com>
2024-02-29 14:13:31 +0000

    shell file

---
+++ Jenkinsfile.sh
@@ -0,0 +1,31 @@
+pipeline {
+    agent any
+    stages{
+        stage("Build"){
+            steps{
+                echo "Building"
+                sh "touch buildfile.txt"
+            }
+        }
+        stage ("Test"){
+            steps{
+                echo "Testing"
+                sh "ls -la"
+            }
+        }
+        stage ("Deploy"){
+            steps{
+                echo "Deploy"
+                sh "pwd"
+                sh "mv buildfile.txt ~/jenkins-pipelines/"
+            }
+        }
+
+
+
+
+
+    }
+
+
+}
